65e44ed99f40d8bbf966bea609dd5a7b1a06a365
agusbudionoprastyo <agna4605@gmail.com>
2024-07-05 15:14:17 +0700

    Update regform.php

--- roleadmin/regform.php
+++ roleadmin/regform.php
@@ -387,7 +387,7 @@ endif;
         // Panggil API di sini
         // Contoh:
         $.ajax({
-            url: 'https://103.236.201.34:3000/replicate',
+            url: 'http://103.236.201.34:3000/replicate',
             method: 'GET',
             success: function(response) {
                 // Handle response
