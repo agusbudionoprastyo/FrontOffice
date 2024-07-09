b73d57d580a196cd04cf66b83e34acb0f87c0b23
agusbudionoprastyo <agna4605@gmail.com>
2024-07-09 10:42:18 +0700

    Update modules-datatables.js

--- assets/js/page/modules-datatables.js
+++ assets/js/page/modules-datatables.js
@@ -37,5 +37,5 @@ $("[data-checkboxes]").each(function () {
 // Initialize DataTable with initial sorting order
 $("#table-2").dataTable({
   columnDefs: [{ sortable: false, targets: [0, 2, 3] }],
-  order: [[1, "desc"]] // Sort by the first column (ID) in descending order initially
+  order: [[4, "desc"]] // Sort by the first column (ID) in descending order initially
 });
