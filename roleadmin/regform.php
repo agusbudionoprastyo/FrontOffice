10b9a4d240365aa4c91fefbe947d9c5595135fe9
agusbudionoprastyo <agna4605@gmail.com>
2024-07-09 14:51:51 +0700

    Update regform.php

--- roleadmin/regform.php
+++ roleadmin/regform.php
@@ -388,7 +388,7 @@ $(document).ready(function(){
 <!-- Bootstrap Datepicker JavaScript -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.10.0/js/bootstrap-datepicker.min.js" integrity="sha512-LsnSViqQyaXpD4mBBdRYeP6sRwJiJveh2ZIbW41EBrNmKxgr/LFZIiWT6yr+nycvhvauz8c2nYMhrP80YhG7Cw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <!-- <script type="text/javascript" src="assets/js/qrCode/jquery.min.js"></script> -->
-<script type="text/javascript" src="assets/js/qrCode/qrcode.js"></script>
+<script type="text/javascript" src="../assets/js/qrCode/qrcode.js"></script>
 <!-- Initialize Datepicker -->
 <script>
     $(document).ready(function(){
