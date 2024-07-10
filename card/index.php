caa88ce8ad558b0c0786574ec5a81dbdeedac17c
agusbudionoprastyo <agna4605@gmail.com>
2024-07-10 14:55:24 +0700

    Update index.php

--- card/index.php
+++ card/index.php
@@ -1,7 +1,7 @@
 <?php
 require_once 'koneksi.php';
 $folio = $_GET['folio'];
-$query = mysqli_query($conn, "SELECT * FROM FOGUEST WHERE folio='$folio' AND foliostatus='I'");
+$query = mysqli_query($conn, "SELECT * FROM FOGUEST WHERE folio='$folio' AND foliostatus<>'O'");
 $data = mysqli_fetch_assoc($query);
 ?>
 <!DOCTYPE html>
