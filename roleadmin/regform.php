acb3da44b82c51aa84e6f03344935695a92cdb98
agusbudionoprastyo <agna4605@gmail.com>
2024-07-10 13:34:14 +0700

    Update regform.php

--- roleadmin/regform.php
+++ roleadmin/regform.php
@@ -42,7 +42,7 @@ require_once '../helper/connection.php';
     padding: 0.375rem 0.75rem;
     font-size: 1rem;
     line-height: 1.5;
-    color: #495057;
+    color: #495057; 
     background-clip: padding-box;
     border: 1px solid #ced4da;
     border-radius: 0.25rem;
@@ -224,7 +224,7 @@ min-width: 250px; /* Kolom GUESTBILL */
                                     </td>
                                     
                                     <td><?php echo $row['fname']; ?></td>
-                                    <td><?php echo $row['folio']; ?><a href="https://ecard.dafam.cloud/?folio=<?php echo $row['folio']; ?>" target="_blank"></a></td>
+                                    <td><?php echo $row['folio']; ?><a href="https://ecard.dafam.cloud/?folio=<?php echo $row['folio']; ?>" target="_blank"><i class="fa-solid fa-envelope-open fa-xl"></i></a></td>
                                     <td><?php echo $row['room']; ?></td>
                                     <td><?php echo $row['roomtype']; ?></td>
                                     <td>
