182ab8c9ec4d05a0ed5a9b1ae1214b0cfda45d04
agusbudionoprastyo <agna4605@gmail.com>
2024-07-10 11:22:49 +0700

    Update regform.php

--- roleadmin/regform.php
+++ roleadmin/regform.php
@@ -203,7 +203,7 @@ min-width: 250px; /* Kolom GUESTBILL */
                                         <?php if (empty($row['at_regform'])): ?>
                                             <button class="btn btn-sm btn-default mb-md-0 mb-1" data-toggle="modal" data-target="#deviceModal" data-id="<?php echo $row['folio']; ?>"><i class="fa-solid fa-paper-plane fa-xl" style="color: #f82b85;"></i></button>
                                         <?php endif; ?>
-                                        <?php if (!empty($row[room])): ?>
+                                        <?php if (!empty($row['room'])): ?>
                                             <button onclick="printQRCode(this);" class="btn btn-default mb-md-0 mb-1"
                                                 data-room="<?php echo htmlspecialchars($row['room']); ?>"
                                                 data-folio="<?php echo htmlspecialchars($row['folio']); ?>">
