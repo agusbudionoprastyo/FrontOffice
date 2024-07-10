02006732e40b7776d34e7c7cd0d370840df2e91e
agusbudionoprastyo <agna4605@gmail.com>
2024-07-10 10:36:57 +0700

    Update index.php

--- card/index.php
+++ card/index.php
@@ -124,7 +124,8 @@ $data = mysqli_fetch_assoc($query);
                 <div class="home__container container grid">
                     <div class="home__data">
                         <span class="home__data-subtitle"><?= !empty($data['pesan']) ? $data['pesan'] : 'Selamat Datang' ?></span>
-                        <h1 class="home__data-title">di Hotel Dafam Semarang <?= trim(explode(',', $data['fname'])[1]) ?> <b><?= trim(explode(',', $data['fname'])[0]) ?></b></h1>
+                        <h1 class="home__data-title">di Hotel Dafam Semarang</h1>
+                        <span class="home__data-subtitle"><?= trim(explode(',', $data['fname'])[1]) . '.' ?> <b><?= trim(explode(',', $data['fname'])[0]) ?></b></span>
                         <div id="container" class="button"><i class="fa-solid fa-file-pdf"></i> Hotel Directory</div>
                         <div id="canting" class="button"><i class="fa-solid fa-file-pdf"></i> F&B Menu </div>
                     </div>
