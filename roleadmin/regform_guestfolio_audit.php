9cd2099bfd93ee8669897ef7e9d41078d22233c7
agusbudionoprastyo <agna4605@gmail.com>
2024-07-12 00:18:14 +0700

    Update regform_guestfolio_audit.php

--- roleadmin/regform_guestfolio_audit.php
+++ roleadmin/regform_guestfolio_audit.php
@@ -9,57 +9,10 @@ require_once '../helper/connection.php';
 <!-- Bootstrap Datepicker CSS -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.10.0/css/bootstrap-datepicker3.min.css" integrity="sha512-aQb0/doxDGrw/OC7drNaJQkIKFu6eSWnVMAwPN64p6sZKeJ4QCDYL42Rumw2ZtL8DB9f66q4CnLIUnAw28dEbg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
-<!-- Custom CSS -->
-<style>
-/* Custom datepicker container */
-.datepicker-container {
-    position: relative;
-    display: inline-block;
-}
-
-/* Custom datepicker input */
-.custom-datepicker-input {
-    width: 150px; /* Adjust width as needed */
-    padding: 0.375rem 0.75rem;
-    font-size: 1rem;
-    line-height: 1.5;
-    color: #495057;
-    background-clip: padding-box;
-    border: 1px solid #ced4da;
-    border-radius: 0.25rem;
-    transition: border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
-}
-
-/* Custom datepicker button */
-.custom-datepicker-button {
-    position: absolute;
-    top: .18rem;
-    right: .5rem;
-    padding: 0.175rem 0.55rem;
-    border-top-right-radius: 0.25rem;
-    border-bottom-right-radius: 0.25rem;
-}
-
-/* Custom datepicker button icon */
-.custom-datepicker-button i {
-    vertical-align: middle;
-}
-
-.button-spacing {
-    margin-bottom: 10px;
-}
-
-/* Sembunyikan teks "Clear" */
-.datepicker{
-    font-size: 12px;
-}
-
-</style>
-
 <section class="section">
   <div class="section-header d-flex justify-content-between">
-    <b><i class="fa-brands fa-slack fa-lg"></i> Audit <i class="fa-solid fa-folder-open"></i> Regcard Guestfolio</b>
-    <!-- <a href="regform_create.php" class="btn btn-dark rounded-pill"><i class="fa-solid fa-circle-plus fa-beat-fade fa-xl"></i> REGCARD</a> -->
+    <b><i class="fa-solid fa-fire"></i> FrontOffice <i class="fa-solid fa-folder-open"></i> Regcard Guestfolio</b>
+    <a href="#" onclick="syncData(); return false;" class="btn btn-dark rounded-pill"><i class= "fa-solid fa-rotate fa-beat-fade"></i> SYNC DATA</a>
   </div>
 
 <!-- Date Filter -->
@@ -79,17 +32,20 @@ require_once '../helper/connection.php';
             <label for="end-date" class="btn btn-light rounded-pill custom-datepicker-button"><i class="fa-solid fa-calendar-days"></i></label>        
         </div>
         <div class="col-auto">
-            <input type="text" class="rounded-pill custom-datepicker-input" id="date_create" name="date_create" autocomplete="off" 
-                   value="<?php echo isset($_GET['date_create']) ? $_GET['date_create'] : ''; ?>" 
+            <input type="text" class="rounded-pill custom-datepicker-input" id="datecreate" name="datecreate" autocomplete="off" 
+                   value="<?php echo isset($_GET['datecreate']) ? $_GET['datecreate'] : ''; ?>" 
                    placeholder="Date Created">
-            <label for="date_create" class="btn btn-info rounded-pill custom-datepicker-button"><i class="fa-solid fa-calendar-days"></i></label>        
+            <label for="datecreate" class="btn btn-light rounded-pill custom-datepicker-button"><i class="fa-solid fa-calendar-days"></i></label>        
         </div>
-        <!-- <div class="col-auto">
-            <button type="submit" class="btn btn-primary rounded-pill" id="filter"><i class="fa-solid fa-circle-check"></i> APPLY</button>
-        </div> -->
+
+        <div class="col-auto">
+            <input type="text" class="rounded-pill custom-search-input" aria-describedby="basic-addon2" id="search-input" placeholder="Search...">
+        </div>
+
         <div class="col-auto">
             <button type="button" class="btn btn-danger rounded-pill" id="reset-filter"><i class="fa-solid fa-filter-circle-xmark"></i></button>
         </div>
+
         </div>
     </div>
 </form>
@@ -102,28 +58,27 @@ require_once '../helper/connection.php';
                     <table class="table table-hover table-striped w-100" id="table-2">
                         <thead>
                             <tr>
-                                <th>ID</th>
+                                <th>STATUS</th>
+                                <th>REGCARD</th>
                                 <th>NAME</th>
-                                <th>DATEOFBIRTH</th>
-                                <th>GENDER</th>
-                                <th>PHONE</th>
-                                <th>EMAIL</th>
                                 <th>FOLIO</th>
                                 <th>ROOM</th>
                                 <th>ROOMTYPE</th>
+                                <th>ROOM STATUS</th>
                                 <th>CHECKIN</th>
                                 <th>CHECKOUT</th>
-                                <th>REGCARD</th>
-                                <th>GUESTFOLIO</th>
+                                <th>DATEOFBIRTH</th>
+                                <th>PHONE</th>
+                                <th>EMAIL</th>
+                                <th>GUESTBILL</th>
                                 <th>CL / VOUCHER</th>
-                                <th>STATUS</th>
-                                <th><i class="fa-brands fa-slack"></i></th>
+                                <th>DATECREATE</th>
                             </tr>
                         </thead>
                         <tbody>
                             <?php
                             // Default SQL query
-                            $sql = "SELECT * FROM regform";
+                            $sql = "SELECT * FROM FOGUEST";
 
                             // Check if start date is provided
                             if (isset($_GET['start_date']) && !empty($_GET['start_date'])) {
@@ -139,13 +94,13 @@ require_once '../helper/connection.php';
                             }
 
                             // Check if create date is provided
-                            if (isset($_GET['date_create']) && !empty($_GET['date_create']) && empty($_GET['start_date']) && empty($_GET['end_date'])) {
-                                $date_create = $_GET['date_create'];
-                                $sql .= " WHERE date_created = '$date_create'";
+                            if (isset($_GET['datecreate']) && !empty($_GET['datecreate']) && empty($_GET['start_date']) && empty($_GET['end_date'])) {
+                                $datecreate = $_GET['datecreate'];
+                                $sql .= " WHERE datecreate = '$datecreate'";
                             }
 
                             // Add ORDER BY clause
-                            $sql .= " ORDER BY id DESC";
+                            $sql .= " ORDER BY folio DESC";
 
                             // Perform the query
                             $result = mysqli_query($connection, $sql);
@@ -159,34 +114,57 @@ require_once '../helper/connection.php';
                             while ($row = mysqli_fetch_array($result)) {
                                 ?>
                                 <tr>
-                                    <td><?php echo $row['id']; ?></td>
-                                    <td><?php echo $row['nama']; ?></td>
-                                    <td><?php echo $row['tempat_tanggal_lahir']; ?></td>
-                                    <td><?php echo $row['jenis_kelamin']; ?></td>
-                                    <td><?php echo $row['no_telp']; ?></td>
-                                    <td><?php echo $row['email']; ?></td>
-                                    <td><?php echo $row['folio']; ?></td>
-                                    <td><?php echo $row['room']; ?></td>
-                                    <td><?php echo $row['roomtype']; ?></td>
-                                    <td><?php echo $row['dateci']; ?></td>
-                                    <td><?php echo $row['dateco']; ?></td>
                                     <td>
-                                        <?php if ($row['at_regform']): ?>
-                                            <a class="btn btn-sm btn-default mb-md-0 mb-1" href="<?php echo $row['at_regform']; ?>" target="_blank"><i class="fa-solid fa-file-pdf fa-xl"></i></a>                                
+                                        <?php if ($row['status'] === '0'): ?>
+                                            <a class="btn btn-sm btn-primary mb-md-0 mb-1" href="audit_checked.php?id=<?php echo $row['id']; ?>"><i class="fa-solid fa-circle-check fa-xl"></i></a>
                                         <?php endif; ?>
-                                        <?php if (empty($row['signature_path'])): ?>
-                                            <a class="btn btn-sm btn-default mb-md-0 mb-1">unsigned <i class="fa-solid fa-circle-exclamation" style="color: #FFD43B;"></i></a>
-                                        <?php endif; ?>
-                                        <?php if ($row['signature_path']): ?>
-                                            <a class="btn btn-sm btn-default mb-md-0 mb-1">signed <i class="fa-solid fa-circle-check" style="color: #63E6BE;"></i></a>
+                                        <?php if ($row['status'] === '1'): ?>
+                                            <a class="btn btn-sm btn-secondary mb-md-0 mb-1"><i class="fa-solid fa-circle-check fa-xl"></i></a>
                                         <?php endif; ?>
                                     </td>
                                     <td>
-                                        <?php if ($row['at_guestfolio']): ?>
-                                            <a class="btn btn-sm btn-default mb-md-0 mb-1" href="<?php echo $row['at_guestfolio']; ?>" target="_blank"><i class="fa-solid fa-file-pdf fa-xl"></i></a>
+                                        <?php if ($row['at_regform']): ?>
+                                            <a class="btn btn-sm btn-default mb-md-0 mb-1" href="<?php echo $row['at_regform']; ?>" target="_blank"><i class="fa-solid fa-file-pdf fa-xl" style="color: #B5120C;"></i></a>                                
                                         <?php endif; ?>
-                                        <?php if (empty($row['at_guestfolio'])): ?>
-                                            <a class="btn btn-sm btn-default mb-md-0 mb-1">not uploaded <i class="fa-solid fa-ban" style="color: #ff0000;"></i></a>
+                                        <?php if (empty($row['room'])): ?>
+                                            <a class="btn btn-sm btn-default mb-md-0 mb-1" href="regform_edit.php?folio=<?php echo $row['folio']; ?>"><i class="fa-regular fa-pen-to-square fa-xl"></i></a>
+                                        <?php endif; ?>
+                                        <?php if (empty($row['rc_signature_path'])): ?>
+                                            <a class="btn btn-sm btn-default mb-md-0 mb-1">unsigned <i class="fa-solid fa-circle-exclamation" style="color: #FFD43B;"></i></a>
+                                        <?php endif; ?>
+                                        <?php if ($row['rc_signature_path']): ?>
+                                            <a class="btn btn-sm btn-default mb-md-0 mb-1">signed <i class="fa-solid fa-circle-check" style="color: #63E6BE;"></i></a>
+                                        <?php endif; ?>
+                                    </td>
+                                    
+                                    <td><?php echo $row['fname']; ?></td>
+                                    <td><a class="btn btn-default" href="https://ecard.dafam.cloud/?folio=<?php echo $row['folio']; ?>" target="_blank"><?php echo $row['folio']; ?></a></td>
+                                    <td><?php echo $row['room']; ?></td>
+                                    <td><?php echo $row['roomtype']; ?></td>
+                                    <td>
+                                        <?php $foliostatus = trim($row['foliostatus']); ?>
+                                        <?php if ($foliostatus == 'I'): ?>
+                                            <span style="color: #36BA98;">inHouse</span>
+                                        <?php elseif ($foliostatus == 'O'): ?>
+                                            <span style="color: #C80036">CheckOut</span>
+                                        <?php elseif ($foliostatus == 'C'): ?>
+                                            <span style="color: #1679AB;">Confirm</span>
+                                        <?php elseif ($foliostatus == 'G'): ?>
+                                            <span style="color: #102C57;">Guarantee</span>
+                                        <?php elseif ($foliostatus == 'T'): ?>
+                                            <span style="color: #43919B;">Tentative</span>
+                                        <?php else: ?>
+                                            <span class="text-muted">Cancel</span>
+                                        <?php endif; ?>
+                                    </td>
+                                    <td><?php echo $row['dateci']; ?></td>
+                                    <td><?php echo $row['dateco']; ?></td>
+                                    <td><?php echo $row['birthday']; ?></td>
+                                    <td><?php echo $row['resv_phone']; ?></td>
+                                    <td><?php echo $row['resv_email']; ?></td>
+                                    <td>
+                                        <?php if ($row['at_guestfolio']): ?>
+                                            <a class="btn btn-sm btn-default mb-md-0 mb-1" href="<?php echo $row['at_guestfolio']; ?>" target="_blank"><i class="fa-solid fa-file-pdf fa-xl" style="color: #B5120C;"></i></a>
                                         <?php endif; ?>
                                         <?php if ((empty($row['g_signature_path'])) && ($row['at_guestfolio'])): ?>
                                             <a class="btn btn-sm btn-default mb-md-0 mb-1">unsigned <i class="fa-solid fa-circle-exclamation" style="color: #FFD43B;"></i></a>
@@ -200,26 +178,8 @@ require_once '../helper/connection.php';
                                             <a class="btn btn-sm btn-default mb-md-0 mb-1" href="<?php echo $row['at_ota_voucher']; ?>" target="_blank"><i class="fa-solid fa-file-zipper fa-xl"></i></a>
                                             <a class="btn btn-sm btn-default mb-md-0 mb-1">uploaded <i class="fa-solid fa-circle-check" style="color: #63E6BE;"></i></a>
                                         <?php endif; ?>
-                                        <?php if (empty($row['at_ota_voucher'])): ?>
-                                            <a class="btn btn-sm btn-default mb-md-0 mb-1">not uploaded <i class="fa-solid fa-ban" style="color: #ff0000;"></i></a></a>
-                                        <?php endif; ?>
                                     </td>
-                                    <td>
-                                        <?php if ($row['status'] === '0'): ?>
-                                            <a class="btn btn-sm btn-default mb-md-0 mb-1">unchecked <i class="fa-solid fa-circle-question" style="color: #ff0000;"></i></a>
-                                        <?php endif; ?>
-                                        <?php if ($row['status'] === '1'): ?>
-                                            <a class="btn btn-sm btn-default mb-md-0 mb-1">checked <i class="fa-solid fa-circle-check" style="color: #63E6BE;"></i></a>
-                                        <?php endif; ?>
-                                    </td>
-                                    <td>
-                                        <?php if ($row['status'] === '0'): ?>
-                                            <a class="btn btn-sm btn-primary mb-md-0 mb-1" href="audit_checked.php?id=<?php echo $row['id']; ?>"><i class="fa-solid fa-circle-check fa-xl"></i></a>
-                                        <?php endif; ?>
-                                        <?php if ($row['status'] === '1'): ?>
-                                            <a class="btn btn-sm btn-secondary mb-md-0 mb-1"><i class="fa-solid fa-circle-check fa-xl"></i></a>
-                                        <?php endif; ?>
-                                    </td>
+                                    <td><?php echo $row['datecreate']; ?></td>
                                 </tr>
                                 <?php
                             }
@@ -236,32 +196,9 @@ require_once '../helper/connection.php';
 <?php
 require_once '../layout/_bottom.php';
 ?>
-
 <!-- Bootstrap Datepicker JavaScript -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.10.0/js/bootstrap-datepicker.min.js" integrity="sha512-LsnSViqQyaXpD4mBBdRYeP6sRwJiJveh2ZIbW41EBrNmKxgr/LFZIiWT6yr+nycvhvauz8c2nYMhrP80YhG7Cw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-
-<!-- Initialize Datepicker -->
-<script>
-    $(document).ready(function(){
-        $('.custom-datepicker-input').datepicker({
-            format: 'yyyy-mm-dd', // Format Tanggal (YYYY-MM-DD)
-            autoclose: true,
-            todayHighlight: true,
-            clearBtn: true // Tampilkan tombol hapus
-        })
-
-        // Ketika nilai input berubah, serahkan formulir
-        $('.custom-datepicker-input').on('change', function() {
-            $('#filter').submit();
-        });
-
-        // Handler untuk tombol reset filter
-        $('#reset-filter').click(function() {
-            $('.custom-datepicker-input').datepicker('setDate', null); // Mengatur tanggal datepicker ke null
-            $('#filter').submit(); 
-        });
-    });
-</script>
+<script type="text/javascript" src="../assets/js/qrCode/qrcode.js"></script>
 
 <!-- Page Specific JS File -->
 <?php
@@ -292,4 +229,5 @@ if (isset($_SESSION['info'])):
   unset($_SESSION['info']);
 endif;
 ?>
+
 <script src="../assets/js/page/modules-datatables.js"></script>
