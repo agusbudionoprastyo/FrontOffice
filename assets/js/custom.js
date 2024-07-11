b693bc5e503cc1b23aad77a24c775b45737f016c
agusbudionoprastyo <agna4605@gmail.com>
2024-07-11 17:07:49 +0700

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -175,7 +175,7 @@ function printDocumentWithQR(room, qrText) {
     printDocument += '<style>body { font-family: Arial, sans-serif; font-size: 6pt; }</style>';
     printDocument += '<style>.label-container { display: flex; align-items: center; justify-content: space-between; height: 100%;}</style>';
     printDocument += '<style>.label-content { flex: 1; }</style>';
-    printDocument += '<style>.label-qr { flex-shrink: 0; }</style>';
+    printDocument += '<style>.label-qr { flex: 1 }</style>';
     printDocument += '</head><body>';
 
     // Container untuk QR code dan detail ROOM, WIFI, PASSWORD dalam satu baris
