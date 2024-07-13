450d329b3f5080ad551490ccef37cccf847a42ff
agusbudionoprastyo <agna4605@gmail.com>
2024-07-13 19:49:33 +0700

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -155,15 +155,15 @@ function printDocumentWithQR(room, qrText) {
     printDocument += '</head><body>';
 
     // Container untuk QR code dan detail ROOM, WIFI, PASSWORD dalam satu baris
-    printDocument += '<div style="float: left; margin-right: 7mm;">';
+    printDocument += '<div style="float: left; margin-right: 5mm;">';
     printDocument += '<div id="qrcodeContainer"></div>'; // Letakkan QR code di dalam container ini
 
     printDocument += '<h3 style="margin: 0;">ROOM ' + room + '</h3>';
     printDocument += '<br><br>';
-    printDocument += '<h3 style="margin: 0;">Wifi</h3>';
-    printDocument += '<h3 style="margin: 0;">dafamsemarang</h3>';
-    printDocument += '<h3 style="margin: 0;">Password</h3>';
-    printDocument += '<h3 style="margin: 0;">krasansare</h3>';
+    // printDocument += '<h3 style="margin: 0;">wifi</h3>';
+    printDocument += '<h3 style="margin: 0;"><i class="fa-solid fa-wifi"></i> dafamsemarang</h3>';
+    // printDocument += '<h3 style="margin: 0;">Password</h3>';
+    printDocument += '<h3 style="margin: 0;"><i class="fa-solid fa-key"></i> krasansare</h3>';
     printDocument += '</div>';
 
     printDocument += '</body></html>';
