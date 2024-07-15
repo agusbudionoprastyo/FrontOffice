050dc6cf60c07bdc7b0f0f534e7e7e99c4a12aa6
agusbudionoprastyo <agna4605@gmail.com>
2024-07-15 17:21:31 +0700

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -219,7 +219,7 @@ function printQRCode(folio, room) {
 
     iframeDocument.write('<div class="label-container">' +
                         '<div class="text-container">' +
-                        '<div class="text"><b>Room ' + room + '</b><br><br><b>Wifi</b> dafam<b><br>Password</b><br>krasansare<br><br></div>' +
+                        '<div class="text"><br><b>Room ' + room + '</b><br><br><b>Wifi</b> dafam<b><br>Password</b><br>krasansare<br><br></div>' +
                         '</div>' +
                         '<div class="qrcode-container"><div class="qrcode"><img src="' + qrCodeUrl + '"></div></div>' +
                         '</div>');
