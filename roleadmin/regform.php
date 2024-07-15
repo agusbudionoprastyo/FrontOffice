a3a519054ef8edbf494d9c3d42c695b1c3022633
agusbudionoprastyo <agna4605@gmail.com>
2024-07-15 09:20:38 +0700

    Update regform.php

--- roleadmin/regform.php
+++ roleadmin/regform.php
@@ -65,7 +65,7 @@ require_once '../helper/connection.php';
                                 <th>REGCARD</th>
                                 <th>NAME</th>
                                 <th>FOLIO</th>
-                                <th data-orderable="false"><input type="hidden" id="selectAllCheckbox"> <label for="selectAllCheckbox"># ROOM</label></input></th>
+                                <th data-orderable="false"><input type="checkbox" id="selectAllCheckbox" style="display: none;"></input><label for="selectAllCheckbox"># ROOM</label></th>
                                 <th>ROOMTYPE</th>
                                 <th>ROOM STATUS</th>
                                 <th>CHECKIN</th>
