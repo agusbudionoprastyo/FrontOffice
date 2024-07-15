7ecbf4e70827b570d9ef327af032ea010c98ce7e
agusbudionoprastyo <agna4605@gmail.com>
2024-07-15 08:15:25 +0700

    Update regform.php

--- roleadmin/regform.php
+++ roleadmin/regform.php
@@ -119,7 +119,6 @@ require_once '../helper/connection.php';
                                         value="<?php echo $row['folio']; ?>"
                                             data-room="<?php echo htmlspecialchars($row['room']); ?>"
                                             data-folio="<?php echo htmlspecialchars($row['folio']); ?>">
-                                        <button type="button" class="btn btn-dark"><i class="fa-solid fa-print fa-xl"></i></button>
                                         <?php if (empty($row['at_regform'])): ?>
                                             <button class="btn btn-sm btn-default mb-md-0 mb-1" data-toggle="modal" data-target="#deviceModal" data-id="<?php echo $row['folio']; ?>"><i class="fa-solid fa-paper-plane fa-xl" style="color: #f82b85;"></i></button>
                                         <?php endif; ?>
