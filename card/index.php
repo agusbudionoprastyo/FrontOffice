3c43dcbff15b79b8d8473b05373e1ce0fbaadaa4
agusbudionoprastyo <97543860+agusbudionoprastyo@users.noreply.github.com>
2024-08-26 16:39:31 +0700

    Update index.php

--- card/index.php
+++ card/index.php
@@ -214,7 +214,7 @@ $data = mysqli_fetch_assoc($query);
                             <?php if (!empty($data['room'])): ?>
                                 <a href="https://cantingfood.my.id/#/menu/dafam-<?= ltrim($data['room'], '0') ?>" class="button3">To Order</a>
                             <?php else: ?>
-                                <a href="javascript:void(0);" onclick="isFirstVisit()" class="button3">To Order</a>
+                                <a href="javascript:void(0);" onclick="showAlert()" class="button3">To Order</a>
                             <?php endif; ?>
                         </div>
                         <div class="about__img-overlay">
@@ -618,7 +618,7 @@ $data = mysqli_fetch_assoc($query);
         // Show alert for room service QR code
         function showAlert() {
             Swal.fire({
-                text: 'Please scan QR for room service, Silakan scan QR untuk room service',
+                text: 'Please reload and input your room number first or scan QR for room service, Silakan muat ulang halaman dan input nomor kamar atau scan QR untuk room service',
                 icon: 'info',
                 backdrop: 'rgba(0,0,0,0.4)',
                 showConfirmButton: false,
