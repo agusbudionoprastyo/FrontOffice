ac6a0155a61c9e1fa21e26e5ad9cc3d0fe15f844
agusbudionoprastyo <97543860+agusbudionoprastyo@users.noreply.github.com>
2024-08-26 16:08:18 +0700

    Update index.php

--- card/index.php
+++ card/index.php
@@ -659,7 +659,7 @@ $data = mysqli_fetch_assoc($query);
                 customClass: {
                     popup: 'rounded',
                     input: 'rounded',
-                    button: 'rounded',
+                    confirmButton: 'rounded',
                 }
             }).then((result) => {
                 if (result.isConfirmed) {
